9e2d0c95c343a10c1216d65f25c8b1106f1f2dc0
a.rivera <a.riveraa99@gmail.com>
2024-04-23 23:26:38 -0500

    Added shopping list and got rid of duplicates

--- js/script.js
+++ js/script.js
@@ -18,3 +18,26 @@ var groceries = [
   "olive oil",
   "mustard"
 ];
+
+console.log(groceries.length);
+
+var deleteDuplicates = function (list) {
+  var cleanList = [];
+  for (const item of list) {
+    if(!cleanList.includes(item)){
+      // if the new array does not have the item, then put item into the end of the new array
+      cleanList.push(item);
+    }
+  } // end of for loop
+  return cleanList;
+};
+
+console.log(deleteDuplicates(groceries));
+
+var newGroceries = deleteDuplicates(groceries);
+console.log(newGroceries.length);
+
+console.log(`This week's shopping list: `);
+newGroceries.forEach((element, index) => {
+  console.log(`#${index + 1} ${element}`);
+});
